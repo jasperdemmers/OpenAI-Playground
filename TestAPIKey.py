f228d687237c3a9b65377eae789db7bbc465b9f0
jasperdemmers <jasper.demmers@hotmail.com>
2024-02-20 13:59:54 +0100

    Test OpenAI Api

---
+++ TestAPIKey.py
@@ -0,0 +1,21 @@
+import openai
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+def check_openai_api_key(api_key):
+    client = openai.OpenAI(api_key=api_key)
+    try:
+        client.models.list()
+    except openai.AuthenticationError:
+        return False
+    else:
+        return True
+
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+
+if check_openai_api_key(OPENAI_API_KEY):
+    print("Valid OpenAI API key.")
+else:
+    print("Invalid OpenAI API key.")
